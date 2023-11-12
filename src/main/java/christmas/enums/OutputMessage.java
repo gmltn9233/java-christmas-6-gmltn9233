285862177e905eea065e38a1b54fa7ce0f6dfec1
이희수 <hslee9234@naver.com>
2023-11-12 22:36:40 +0900

    Feat: OutputMessage enums 추가

--- src/main/java/christmas/enums/OutputMessage.java
+++ src/main/java/christmas/enums/OutputMessage.java
@@ -1,8 +1,14 @@
 package christmas.enums;
 
 public enum OutputMessage {
-    DISPLAY_VISIT_DATE("일에 우테코 식당에서 받을 이벤트 헤택 미리 보기!");
-
+    VISIT_DATE("일에 우테코 식당에서 받을 이벤트 헤택 미리 보기!"),
+    ORDER_MENU("<주문 메뉴>"),
+    BEFORE_DISCOUNT_AMOUNT("<할인 전 총주문 금액>"),
+    GIFT_MENU("<증정 메뉴>"),
+    BENEFIT_DETAILS("<혜택 내역>"),
+    TOTAL_BENEFIT_AMOUNT("<총혜택 금액>"),
+    AFTER_DISCOUNT_AMOUNT("<할인 후 예상 결제 금액>"),
+    EVENT_BADGE("<12월 이벤트 배지>");
     String message;
     OutputMessage(String message){
         this.message = message;
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,14 +4,15 @@ import christmas.enums.OutputMessage;
 import christmas.model.OrderMenu;
 import christmas.model.Receipt;
 
-import java.util.List;
 
 public class OutputView {
     public static void displayOrderList(Receipt receipt){
         displayVisitDate(receipt);
+        System.out.println(OutputMessage.ORDER_MENU);
         for(OrderMenu orderMenu : receipt.getOrderMenus() ){
             displayOrder(orderMenu);
         }
+        System.out.println();
     }
     private static void displayOrder(OrderMenu orderMenu){
         String name = orderMenu.getName();
@@ -19,6 +20,6 @@ public class OutputView {
         System.out.println(name+" "+quantity);
     }
     private static void displayVisitDate(Receipt receipt){
-        System.out.println("12월 "+receipt.getVisitDate()+OutputMessage.DISPLAY_VISIT_DATE);
+        System.out.println("12월 "+receipt.getVisitDate()+OutputMessage.VISIT_DATE+"\n");
     }
 }
